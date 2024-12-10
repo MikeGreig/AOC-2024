f4c116879fde00aef51d1c5fd6dfcd82a5d80c6d
mike <mikeguygreig@gmail.com>
2024-12-09 21:47:10 -0800

    add python solution 1.1

---
+++ 01/01.py
@@ -0,0 +1,7 @@
+input_pairs = [[line.strip().split()[0], line.strip().split()[1]] for line in open("input.txt")]
+list_a, list_b = sorted([pair[0] for pair in input_pairs]), sorted([pair[1] for pair in input_pairs])
+compare_list = zip(list_a, list_b)
+total_distance = 0
+for pair in compare_list:
+    total_distance += abs(int(pair[1]) - int(pair[0]))
+print(total_distance)
